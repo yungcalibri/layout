e60688c34a66aecb997d5e488a43f4f9d7b9ed30
yung calibri <yungcalibri@protonmail.com>
2022-10-11 20:45:32 -0400

    import css in index.js for bundler

--- index.js
+++ index.js
@@ -10,3 +10,5 @@ import Reel     from './Reel/Reel.js';
 import Sidebar  from './Sidebar/Sidebar.js';
 import Stack    from './Stack/Stack.js';
 import Switcher from './Switcher/Switcher.js';
+
+import './global.css';
